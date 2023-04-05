3ab727f915d8692c5f6831ce90482021a192b2db
NetilogyIvan <inkornelyuk@gmail.com>
2023-04-05 23:03:12 +0600

    added tests

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,3 +1,61 @@
 package ru.netology.stats;
+
 public class StatsService {
+    public int minSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i;// запомним его как минимальный
+
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public long total(long[] sales) {
+        long sumSale = 0;
+        for (long sale : sales) {
+            sumSale += sale;
+        }
+        return sumSale;
+    }
+
+    public long averageSales(long[] sales) {
+        long totalSales = total(sales);
+        long averageSales = totalSales / 12;
+        return averageSales;
+    }
+
+    public int monthSalesBelowAverage(long[] sales) {
+        int counter = 0;
+        long averageSales = averageSales(sales);
+        for (long sale : sales) {
+            if (sale < averageSales(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthSalesHigherAverage(long[] sales) {
+        int counter = 0;
+        long averageSales = averageSales(sales);
+        for (long sale : sales) {
+            if (sale > averageSales(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,3 +1,79 @@
 package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class StatsServiceTest {
+    @Test
+    void numberMonthsMinSales() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.minSales(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void numberMonthsMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.maxSales(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxSales() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.total(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void averageSales() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.averageSales(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void monthSalesBelowAverage() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.monthSalesBelowAverage(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void monthSalesHigherAverage() {
+        StatsService service = new StatsService();
+
+        long[] dataSalesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.monthSalesHigherAverage(dataSalesByMonths);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
